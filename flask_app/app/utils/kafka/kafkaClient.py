7f946d177f54b95b993f6168e5e0fcba196cb7c2
tamirpa <tamirpa@cloudzone.io>
2024-06-01 15:36:54 +0300

    create consume messages method

--- flask_app/app/utils/kafka/kafkaClient.py
+++ flask_app/app/utils/kafka/kafkaClient.py
@@ -1,5 +1,5 @@
 from confluent_kafka import Producer, Consumer, KafkaError
-from app.config import Config
+from config import Config
 
 class KafkaClient:
     def __init__(self, bootstrap_servers):
@@ -10,7 +10,40 @@ class KafkaClient:
         pass
 
     def consume_messages(self, topic: str) -> None:
-        pass
+
+        consumer = Consumer({
+            'bootstrap.servers': self.bootstrap_servers,
+            'group.id': 'foo',
+            'auto.offset.reset': 'earliest'
+        })
+
+        consumer.subscribe([topic])
+
+        try:
+            while True:
+                msg = consumer.poll(1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        continue
+                    else:
+                        print(msg.error())
+                        break
+                else:
+                    print('Received message: {}'.format(msg.value().decode('utf-8')))
+        except KeyboardInterrupt:
+            pass
+        finally:
+            consumer.close()
 
     def create_topic(self, topic_name: str, num_partitions: int = 1, replication_factor: int = 1) -> None:
         pass
+
+# Example usage:
+if __name__ == "__main__":
+    
+    kafka_client = KafkaClient('localhost:9092')
+    
+    # Consume messages from a topic
+    kafka_client.consume_messages('orders')
