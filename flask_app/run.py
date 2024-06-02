993c04538dcd69d48c5dd02f03091e0fd689686d
tamirpa <tamirpa@cloudzone.io>
2024-06-03 00:36:14 +0300

    add check if kafka topic exists if not create it

--- flask_app/run.py
+++ flask_app/run.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from app.api.v1.orders_api import orders_bp
 from app.utils.sql.db import db
+from app.config import Config
 import logging
-
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka import KafkaException
 
 def create_app():
     app = Flask(__name__)
@@ -28,6 +30,26 @@ def create_app():
         db.execute_query(schema)
     except Exception as e:
         logging.error("[run.create_app] Error occurred while initializing database schema")
+
+    # Create an instance of KafkaAdminClient
+    admin_client = AdminClient({'bootstrap.servers':Config.KAFKA_BOOTSTRAP_SERVERS})
+
+    # Check if the topic exists 
+    try:
+        topics = admin_client.list_topics().topics
+    except KafkaException as e:
+        logging.error(f"Failed to list topics: {e}")
+
+    if Config.KAFKA_TOPIC in topics:
+        logging.info(f"Topic '{Config.KAFKA_TOPIC}' already exists.")
+    else:
+        # Create the topic if it doesn't exist
+        try:
+            new_topic = NewTopic(Config.KAFKA_TOPIC, num_partitions=1, replication_factor=1)
+            admin_client.create_topics([new_topic])
+            logging.info(f"Topic '{Config.KAFKA_TOPIC}' created successfully.")
+        except Exception:
+            logging.error("Failed to create topic.")
 
     return app
 
