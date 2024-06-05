7ebc20c04f484aa5a1b946a078dd73ea5967ac90
tamirpa <tamirpa@cloudzone.io>
2024-06-05 16:20:49 +0300

    update serializers to handle order msg and optype

--- flask_app/app/utils/kafka/kafka_messages/serializers/proto_order_serializer.py
+++ flask_app/app/utils/kafka/kafka_messages/serializers/proto_order_serializer.py
@@ -1,21 +1,26 @@
 import logging
 from app.dao.orders_dao import Order
-from app.utils.kafka.kafka_messages.serializers.generated_protobuf import order_event_pb2
+from app.utils.kafka.kafka_messages.serializers.generated_protobuf.order_event_pb2 import ProtoOrder, OperationType, OrderMessage
 
 class ProtoOrderSerializer:
     
     @staticmethod
-    def serialize_order(order: Order) -> bytes:
+    def serialize_order(order: Order, operation_type: OperationType) -> bytes:
         try:
-            logging.debug(f"Oder to serialize: {order.to_dict()}")
-            proto_order = order_event_pb2.Order()
+            logging.debug(f"Proto Order to serialize: {order.to_dict()}")
+            proto_order =ProtoOrder()
             proto_order.id = order.id
             proto_order.customer_id = order.customer_id
             proto_order.product_ids = order.product_ids
             proto_order.created_date = order.created_date
             proto_order.updated_date = order.updated_date
+            
+            order_message = OrderMessage(
+                operation_type = operation_type,
+                proto_order = proto_order
+            )
 
-            serialized_order = proto_order.SerializeToString()
+            serialized_order = order_message.SerializeToString()
             logging.debug("Order serialized successfully.")
             return serialized_order
         except Exception as e:
@@ -23,12 +28,12 @@ class ProtoOrderSerializer:
             raise
 
     @staticmethod
-    def deserialize_order(msg: bytes) -> order_event_pb2.Order:
+    def deserialize_order(msg: bytes) -> OrderMessage:
         try:
-            proto_order = order_event_pb2.Order()
-            proto_order.ParseFromString(msg)
+            order_message = OrderMessage()
+            order_message.ParseFromString(msg)
             logging.debug("Order deserialized successfully.")
-            return proto_order
+            return order_message
         except Exception as e:
             logging.error(f"Error deserializing order: {e}")
             raise
