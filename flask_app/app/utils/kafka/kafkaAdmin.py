9e17c43162c5046eb7b89942bd0707743bfa64fa
tamirpa <tamirpa@cloudzone.io>
2024-06-08 19:39:34 +0300

    modify class

--- flask_app/app/utils/kafka/kafkaAdmin.py
+++ flask_app/app/utils/kafka/kafkaAdmin.py
@@ -1,14 +1,14 @@
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka import KafkaException
-from app.config import Config
 import logging
 
 class kafkaAdmin():
+    def __init__(self, kafka_bootstrap_servers: str):
+        self.kafka_bootstrap_servers = kafka_bootstrap_servers
     
-    @staticmethod
-    def init_topic():
+    def init_topic(self, topic: str):
         # Create an instance of KafkaAdminClient
-        admin_client = AdminClient({'bootstrap.servers':Config.KAFKA_BOOTSTRAP_SERVERS})
+        admin_client = AdminClient({'bootstrap.servers':self.kafka_bootstrap_servers})
 
         # Check if the topic exists 
         try:
@@ -16,13 +16,16 @@ class kafkaAdmin():
         except KafkaException as e:
             logging.error(f"Failed to list topics: {e}")
 
-        if Config.KAFKA_TOPIC in topics:
-            logging.info(f"Topic '{Config.KAFKA_TOPIC}' already exists.")
+        if topic in topics:
+            logging.info(f"Topic '{topic}' already exists.")
+            return False
         else:
             # Create the topic if it doesn't exist
+            logging.info(f"Topic '{topic}' doesn't exists.")
             try:
-                new_topic = NewTopic(Config.KAFKA_TOPIC, num_partitions=1, replication_factor=1)
+                new_topic = NewTopic(topic, num_partitions=1, replication_factor=1)
                 admin_client.create_topics([new_topic])
-                logging.info(f"Topic '{Config.KAFKA_TOPIC}' created successfully.")
+                logging.info(f"Topic '{topic}' created successfully.")
+                return True
             except Exception:
                 logging.error("Failed to create topic.")
