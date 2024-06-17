0dc2d7a7cd6d2b2965ca23ced233c096457bb757
tamirpa <tamirpa@cloudzone.io>
2024-06-17 17:37:20 +0300

    add validating kafka msg in tests

--- flask_app/tests/integration/test_orders_api.py
+++ flask_app/tests/integration/test_orders_api.py
@@ -1,8 +1,31 @@
+import json
 import logging
+import time
 
 base_url = "/api/v1/order/"
 
-def test_create_order(client):
+
+def validate_kafka_message(kafka_consumer, expected_order):
+    from flask_app.app.utils.kafka.kafka_messages.serializers.proto_order_serializer import ProtoOrderSerializer
+    from flask_app.app.utils.kafka.kafka_messages.serializers.generated_protobuf.order_event_pb2 import OperationType
+
+    msg = None
+    latest_msg = None
+    while True:
+        msg = kafka_consumer.poll(1.0)
+        if msg is not None:
+            latest_msg = ProtoOrderSerializer.deserialize_order(msg.value())
+        else:
+            break
+    logging.info(
+        f"[validate_kafka_message] Kafka consumed message: {str(OperationType.Name(latest_msg.operation_type))} - {latest_msg.proto_order}")
+
+    assert expected_order['order']['id'] == latest_msg.proto_order.id
+    assert expected_order['order']['customer_id'] == latest_msg.proto_order.customer_id
+    assert expected_order['order']['product_ids'] == latest_msg.proto_order.product_ids
+
+
+def test_create_order(client, kafka_consumer):
     # Define a payload for creating a new order
     payload = {
         "customer_id": "customer_123",
@@ -25,8 +48,39 @@ def test_create_order(client):
     assert get_response['order']['product_ids'] == post_response['order']['product_ids']
     # assert get_response['order']['created_date'] == post_response['order']['created_date']
     # assert get_response['order']['updated_date'] == post_response['order']['updated_date']
+    validate_kafka_message(kafka_consumer, get_response)
 
-    # validate kafaka message was produced by consuming it.
+
+def test_update_order(client, kafka_consumer):
+    payload = {
+        "customer_id": "customer_123",
+        "product_ids": "product_1"
+    }
+    post_response = client.post(base_url, json=payload)
+    post_response = post_response.get_json()
+
+    update_payload = {
+        "customer_id": "customer_123",
+        "product_ids": "product_2"
+    }
+
+    logging.info("[test_create_order] Sending PUT request to update an order")
+    put_response = client.put(f"{base_url}{post_response['order']['id']}", json=update_payload)
+
+    logging.info("[test_create_order] Validate the response code")
+    assert put_response.status_code == 200, "Failed to update order. Unexpected status code."
+    put_response = put_response.get_json()
+
+    logging.info("[test_create_order] Validate the order updated as expected. ")
+    get_response = client.get(f"{base_url}{put_response['order']['id']}")
+    get_response = get_response.get_json()
+
+    assert get_response['order']['id'] == put_response['order']['id']
+    assert get_response['order']['customer_id'] == put_response['order']['customer_id']
+    assert get_response['order']['product_ids'] == put_response['order']['product_ids']
+    # assert get_response['order']['created_date'] == put_response['order']['created_date']
+    # assert get_response['order']['updated_date'] == put_response['order']['updated_date']
+    validate_kafka_message(kafka_consumer, get_response)
 
 
 def test_get_order_order_not_found(client):
@@ -57,14 +111,9 @@ def test_update_order_order_not_found(client):
 def test_delete_order_order_not_found(client):
     # Define an order ID for deleting
     order_id = "894fc9e9-d316-4092-ba1d-4f4dc41e5371"
-    
+
     # Send a DELETE request to delete the order
     logging.info("Sending DELETE request to delete order with ID: %s", order_id)
     response = client.delete(f"{base_url}{order_id}")
 
     assert response.status_code == 404, "Failed to delete order.Unexpected status code."
-
-
-
-
-
