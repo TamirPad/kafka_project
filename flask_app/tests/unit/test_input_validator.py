b899b22a1b2de54a74d7c227c53faf3644891842
tamirpa <tamirpa@cloudzone.io>
2024-06-18 19:58:43 +0300

    create template for input_validator unittests

---
+++ flask_app/tests/unit/test_input_validator.py
@@ -0,0 +1,49 @@
+# import unittest
+# # from flask_app.app.api.v1.input_validator import InputValidator
+#
+#
+# class TestInputValidator(unittest.TestCase):
+#
+#     def test_validate_create_order_input_success(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_create_order_input_empty_payload(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_create_order_input_missing_customer_id(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_create_order_input_missing_product_ids(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_create_order_input_invalid_product_ids(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_update_order_input_success(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_update_order_input_empty_payload(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_update_order_input_invalid_customer_id(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_update_order_input_invalid_product_ids(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_order_id_success(self):
+#         # Add your test code here
+#         pass
+#
+#     def test_validate_order_id_empty(self):
+#         # Add your test code here
+#         pass
